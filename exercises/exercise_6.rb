4a2a46f527cb53610b41571d17ef3ff3f7badfea
RAJINIA <rajini1608@gmail.com>
2022-01-07 10:35:25 -0600

    One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,32 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram",
+  last_name: "Virani",
+  hourly_rate: 60
+  )
+
+@store1.employees.create(
+  first_name: "Rajini",
+  last_name: "A",
+  hourly_rate: 50
+  )
+
+@store1.employees.create(
+  first_name: "Avyukkt",
+  last_name: "Amithap",
+  hourly_rate: 100
+  )
+
+@store2.employees.create(
+  first_name: "Shawshanklh",
+  last_name: "Ami",
+  hourly_rate: 150
+  )
+
+@store2.employees.create(
+  first_name: "Amithap",
+  last_name: "S",
+  hourly_rate: 200
+  )
